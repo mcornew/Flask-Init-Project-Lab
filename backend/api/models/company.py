7bf204d605767da06dff665471a92228ccca1999
Michael Cornew <mcornew@gmail.com>
2023-03-26 09:17:39 -0700

    third_commit

---
+++ backend/api/models/company.py
@@ -0,0 +1,11 @@
+class Company():
+    __table__ = 'companies'
+    columns = ['id', 'foursquare_id', 'name', 'price',
+            'rating', 'likes', 'menu_url']
+
+    def __init__(self, **kwargs):
+        for key in kwargs.keys():
+            if key not in self.columns:
+                raise ValueError(f'{key} not in columns: {self.columns}')
+        for k, v in kwargs.items():
+            setattr(self, k, v)
